c89cf1ed5c216259f616e745ab8f692f6de3ca66
kenny <pipikeny@gmail.com>
2019-07-30 22:30:14 +0800

    fix bug

--- BudgetApp/BudgetApp/BudgetService.cs
+++ BudgetApp/BudgetApp/BudgetService.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BudgetApp
 {
@@ -49,25 +46,37 @@ namespace BudgetApp
             }
             else
             {
-                var firstmonth = budgets.FirstOrDefault(x => x.YearMonth == startDate.ToString("yyyyMM")).Amount /
-                    DateTime.DaysInMonth(startDate.Year, startDate.Month) *
-                    (DateTime.DaysInMonth(startDate.Year, startDate.Month) - startDate.Day + 1);
-                var secondmonth = budgets.FirstOrDefault(x => x.YearMonth == endDate.ToString("yyyyMM")).Amount /
-                    DateTime.DaysInMonth(endDate.Year, endDate.Month) * (endDate.Day);
-
-                var totalAmount = firstmonth + secondmonth;
-                var allStartMonth = new DateTime(startDate.Year, startDate.Month, 1).AddMonths(1);
-                var allEndMonth = new DateTime(endDate.Year, endDate.Month, 1);
-                while (allEndMonth > allStartMonth)
+                var startBudget = budgets.FirstOrDefault(x => x.YearMonth == startDate.ToString("yyyyMM"));
+                int firstmonth = 0;
+                if (startBudget != null)
                 {
-                    searchMonth = allStartMonth.ToString("yyyyMM");
-                    if (budgets.Any(x => x.YearMonth == searchMonth))
-                        totalAmount += budgets.FirstOrDefault(x => x.YearMonth == searchMonth).Amount;
-
-                    allStartMonth = allStartMonth.AddMonths(1);
+                    firstmonth = startBudget.Amount /
+                                 DateTime.DaysInMonth(startDate.Year, startDate.Month) *
+                                 (DateTime.DaysInMonth(startDate.Year, startDate.Month) - startDate.Day + 1);
                 }
 
-                return totalAmount;
+                var endBudget = budgets.FirstOrDefault(x => x.YearMonth == endDate.ToString("yyyyMM"));
+
+                int secondmonth = 0;
+                if (endBudget != null)
+                {
+                    secondmonth = endBudget.Amount /
+                                  DateTime.DaysInMonth(endDate.Year, endDate.Month) * (endDate.Day);
+                }
+
+                var totalAmount = firstmonth + secondmonth;
+                    var allStartMonth = new DateTime(startDate.Year, startDate.Month, 1).AddMonths(1);
+                    var allEndMonth = new DateTime(endDate.Year, endDate.Month, 1);
+                    while (allEndMonth > allStartMonth)
+                    {
+                        searchMonth = allStartMonth.ToString("yyyyMM");
+                        if (budgets.Any(x => x.YearMonth == searchMonth))
+                            totalAmount += budgets.FirstOrDefault(x => x.YearMonth == searchMonth).Amount;
+
+                        allStartMonth = allStartMonth.AddMonths(1);
+                    }
+
+                    return totalAmount;
             }
         }
     }
